7decbaedd9ba2a03cdaffe6054a3196cbbcdf6ce
Henrique Morais <rikeaju@hotmail.com>
2022-05-14 18:42:54 -0300

    P01 - Lesson 2.2 - Comparando Variáveis
    Nessa aula o professor abordou o parse de tipo de uma variável após explicar que a função input recebe o valor no formato String. Como nossa variável de comparação é um integer, tivemos que converter.

--- P01/divination.py
+++ P01/divination.py
@@ -1,1 +1,18 @@
+print("*******************************")
 print("Welcome to the Divination Game!")
+print("*******************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+print("Você digitou: ", chute_str)
+
+chute = int(chute_str)
+
+if numero_secreto == chute:
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim do Jogo!")
