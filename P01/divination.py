5f5e8de173e29e770d771fb4b1fd27d1e8bbe60d
Henrique Morais <rikeaju@hotmail.com>
2022-05-14 20:34:14 -0300

    P01 - Lesson 5.1

--- P01/divination.py
+++ P01/divination.py
@@ -6,7 +6,7 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while rodada <= total_de_tentativas:
+for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada} de {total_de_tentativas}")
     chute_str = input("Digite o seu número: ")
     print("Você digitou: ", chute_str)
