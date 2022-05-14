7ac152dd80236dfa4f522f7d8b9cf4209277c58b
Henrique Morais <rikeaju@hotmail.com>
2022-05-14 19:30:23 -0300

    P01 - Lesson 4.1 - O Laço com While
    Nessa aula foi ensinado o uso do Loop com While.

--- P01/divination.py
+++ P01/divination.py
@@ -3,23 +3,25 @@ print("Welcome to the Divination Game!")
 print("*******************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+while rodada <= total_de_tentativas:
+    print(f"Tentativa {rodada} de {total_de_tentativas}")
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-print("Você digitou: ", chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-chute = int(chute_str)
+    if acertou:
+        print("Você acertou!")
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior do que o número secreto!")
+        elif menor:
+            print("Você errou! O seu chute foi menor do que o número secreto")
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if acertou:
-    print("Você acertou!")
-else:
-    if maior:
-        print("Você errou! O seu chute foi maior do que o número secreto!")
-    elif menor:
-        print("Você errou! O seu chute foi menor do que o número secreto")
-
-print("Fim do Jogo!")
+    rodada = rodada + 1
