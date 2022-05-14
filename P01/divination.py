47bac447418115c8b64e278c885f976a596079fd
Henrique Morais <rikeaju@hotmail.com>
2022-05-14 20:46:01 -0300

    P01 - Lesson 4.4 - Encerrando a Interação e o Loop

--- P01/divination.py
+++ P01/divination.py
@@ -8,9 +8,13 @@ rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada} de {total_de_tentativas}")
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
+
+    if chute < 1 or chute >= 100:
+        print("Você desve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,10 +22,11 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if acertou:
         print("Você acertou!")
+        break
     else:
         if maior:
             print("Você errou! O seu chute foi maior do que o número secreto!")
         elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto")
 
-    rodada = rodada + 1
+print("Fim do Jogo!")
