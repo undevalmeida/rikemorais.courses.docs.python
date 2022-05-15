b0cb5a3f4860fc52e74c71a20ba035fd013e6253
Henrique Morais <rikeaju@hotmail.com>
2022-05-15 12:48:08 -0300

    P02 - Melhorando o Código e Apresentação

--- P02/Games/wordwall.py
+++ P02/Games/wordwall.py
@@ -2,10 +2,44 @@ import random
 
 
 def play():
+
+    oppening()
+    secret = loadWords()
+    hits = loadLetters(secret)
+    print(hits)
+
+    over = False
+    right = False
+    errors = 0
+
+    while not over and not right:
+        kick = getKick()
+
+        if kick in secret:
+            markHit(secret, kick, hits)
+        else:
+            errors += 1
+            draw(errors)
+
+        over = errors == 6
+        right = "_" not in hits
+        print(hits)
+
+    if right:
+        win()
+    else:
+        lost(secret)
+
+    print("End Game!")
+
+
+def oppening():
     print("*******************************")
     print("Welcome to the Divination Game!")
     print("*******************************")
 
+
+def loadWords():
     file = open("words.txt", "r")
     words = []
 
@@ -15,36 +49,110 @@ def play():
     file.close()
     number = random.randrange(0, len(words))
     secret = words[number].upper()
+    return secret
 
-    hits = ["_" for letter in secret]
 
-    over = False
-    right = False
-    errors = 0
+def loadLetters(secret):
+    return ["_" for _ in secret]
 
-    print(hits)
 
-    while not over and not right:
-        kick = input("What's the letter?").strip().upper()
+def getKick():
+    kick = input("What's the letter?").strip().upper()
+    return kick
 
-        if kick in secret:
-            index = 0
-            for letter in secret:
-                if kick == letter:
-                    hits[index] = letter
-                index += 1
-        else:
-            errors += 1
 
-        over = errors == 6
-        right = "_" not in hits
-        print(hits)
+def markHit(secret, kick, hits):
+    index = 0
+    for letter in secret:
+        if kick == letter:
+            hits[index] = letter
+        index += 1
 
-    if right:
-        print("You Win!")
-    else:
-        print("You lost!")
-    print("End Game!")
+
+def win():
+    print("You Win!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+
+def lost(secret):
+    print("You Lost!")
+    print(f"A palavra era {secret}")
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def draw(errors):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if errors == 1:
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if errors == 2:
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if errors == 3:
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if errors == 4:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if errors == 5:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if errors == 6:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if errors == 7:
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if __name__ == "__main__":
