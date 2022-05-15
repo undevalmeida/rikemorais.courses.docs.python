1b8730d3793e6af77b2145020f561daba29c7ad8
Henrique Morais <rikeaju@hotmail.com>
2022-05-14 22:05:37 -0300

    P01 - Lesson 7.2 - Definindo uma Pontuação para o Usuário

--- P01/divination.py
+++ P01/divination.py
@@ -5,7 +5,8 @@ print("Welcome to the Divination Game!")
 print("*******************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+pontos = 1000
 
 print("Qual o nílvel de dificuldade?")
 print("(1) Fácil (2) Médio (3) Difícil")
@@ -21,6 +22,7 @@ else:
 
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada} de {total_de_tentativas}")
+
     chute = int(input("Digite um número entre 1 e 100: "))
     print("Você digitou: ", chute)
 
@@ -33,12 +35,14 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if acertou:
-        print("Você acertou!")
+        print(f"Você acertou e fez {pontos} pontos")
         break
     else:
         if maior:
             print("Você errou! O seu chute foi maior do que o número secreto!")
         elif menor:
             print("Você errou! O seu chute foi menor do que o número secreto")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("Fim do Jogo!")
