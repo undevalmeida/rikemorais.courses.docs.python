fc36ae8e1ae90ee8893d20fccde5b2484a7a1cbd
Henrique Morais <rikeaju@hotmail.com>
2022-05-15 09:59:46 -0300

    P02 - Manipulando Strings

--- P02/Games/wordwall.py
+++ P02/Games/wordwall.py
@@ -9,6 +9,14 @@ def play():
     right = False
 
     while not over and not right:
+        kick = input("What's the letter?").strip().upper()
+
+        index = 0
+        for letter in secret:
+            if kick == letter.upper():
+                print(f"Encontrei a letra {letter.upper()} na posição {index}")
+            index = index + 1
+
         print("Jogando!")
 
     print("Fim do Jogo!")
