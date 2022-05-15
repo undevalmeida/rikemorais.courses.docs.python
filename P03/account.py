0fd2cc639f801982cb89c0f466e2133cd1aa3c70
Henrique Morais <rikeaju@hotmail.com>
2022-05-15 17:40:42 -0300

    P03 - Métodos Privados e Estáticos

--- P03/account.py
+++ P03/account.py
@@ -6,6 +6,7 @@ class Account:
         self.__holder = holder
         self.__balance = balance
         self.__limit = limit
+        self.__bank_code = "001"
 
     def extract(self):
         print(f"The total balance is {self.__balance} from the holder {self.__holder}")
@@ -14,7 +15,14 @@ class Account:
         self.__balance += value
 
     def withdraw(self, value):
-        self.__balance -= value
+        if self.__release_loot(value):
+            self.__balance -= value
+        else:
+            print(f"Amount {value} has exceeded your withdrawal limit.")
+
+    def __release_loot(self, withdrawal_amount):
+        available_value = self.__balance + self.__limit
+        return withdrawal_amount <= available_value
 
     def transfer(self, value, receiver):
         self.withdraw(value)
@@ -35,3 +43,15 @@ class Account:
     @limit.setter
     def limit(self, limit):
         self.__limit = limit
+
+    @staticmethod
+    def bank_code():
+        return "001"
+
+    @staticmethod
+    def bank_codes():
+        return {
+            'BB': '001',
+            'Caixa': '104',
+            'Bradesco': '237'
+        }
