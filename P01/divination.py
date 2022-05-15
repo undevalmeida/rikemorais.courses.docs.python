a5fe8489cb7907962244abdba4dfa80921fca8f6
Henrique Morais <rikeaju@hotmail.com>
2022-05-14 21:55:53 -0300

    P01 - Lesson 7.1 - Adicionando Níveis ao Jogo

--- P01/divination.py
+++ P01/divination.py
@@ -7,11 +7,22 @@ print("*******************************")
 numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
 
+print("Qual o nílvel de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
 for rodada in range(1, total_de_tentativas + 1):
     print(f"Tentativa {rodada} de {total_de_tentativas}")
-    chute_str = input("Digite um número entre 1 e 100: ")
-    print("Você digitou: ", chute_str)
-    chute = int(chute_str)
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Você digitou: ", chute)
 
     if chute < 1 or chute >= 100:
         print("Você desve digitar um número entre 1 e 100!")
