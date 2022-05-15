0f0c6a9fa3f4ce50e1ef4fa6cb10efb21314d641
Henrique Morais <rikeaju@hotmail.com>
2022-05-15 11:14:46 -0300

    P02 - Implementando o Encerramento do Jogo

--- P02/Games/wordwall.py
+++ P02/Games/wordwall.py
@@ -3,26 +3,36 @@ def play():
     print("Welcome to the Divination Game!")
     print("*******************************")
 
-    secret = "banana"
-    hits = ["_", "_", "_", "_", "_", "_"]
+    secret = "rike".upper()
+    hits = ["_" for letter in secret]
 
     over = False
     right = False
+    errors = 0
 
     print(hits)
 
     while not over and not right:
         kick = input("What's the letter?").strip().upper()
 
-        index = 0
-        for letter in secret:
-            if kick == letter.upper():
-                hits[index] = letter
-            index = index + 1
+        if kick in secret:
+            index = 0
+            for letter in secret:
+                if kick == letter:
+                    hits[index] = letter
+                index += 1
+        else:
+            errors += 1
 
+        over = errors == 6
+        right = "_" not in hits
         print(hits)
 
-    print("Fim do Jogo!")
+    if right:
+        print("You Win!")
+    else:
+        print("You lost!")
+    print("End Game!")
 
 
 if __name__ == "__main__":
