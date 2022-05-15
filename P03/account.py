bc03f2f74b5fa679133f7afb5ce2205ed7f3bf65
Henrique Morais <rikeaju@hotmail.com>
2022-05-15 15:00:44 -0300

    P03 - Implementando Métodos

--- P03/account.py
+++ P03/account.py
@@ -6,3 +6,12 @@ class Account:
         self.holder = holder
         self.balance = balance
         self.limit = limit
+
+    def extract(self):
+        print(f"The total balance is {self.balance} from the holder {self.holder}")
+
+    def deposit(self, value):
+        self.balance += value
+
+    def withdraw(self, value):
+        self.balance -= value
