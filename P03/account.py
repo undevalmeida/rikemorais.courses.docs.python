84de5b7fc2531d36e7eb60f017eb80e9daed689f
Henrique Morais <rikeaju@hotmail.com>
2022-05-15 15:48:56 -0300

    P03 - Encapsulamento

--- P03/account.py
+++ P03/account.py
@@ -2,16 +2,20 @@ class Account:
 
     def __init__(self, number, holder, balance, limit):
         print(f"Building the object {self}")
-        self.number = number
-        self.holder = holder
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__holder = holder
+        self.__balance = balance
+        self.__limit = limit
 
     def extract(self):
-        print(f"The total balance is {self.balance} from the holder {self.holder}")
+        print(f"The total balance is {self.__balance} from the holder {self.__holder}")
 
     def deposit(self, value):
-        self.balance += value
+        self.__balance += value
 
     def withdraw(self, value):
-        self.balance -= value
+        self.__balance -= value
+
+    def transfer(self, value, receiver):
+        self.withdraw(value)
+        receiver.deposit(value)
