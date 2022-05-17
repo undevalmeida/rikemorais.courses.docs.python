c784b0f198c25b6118a07fb7faa2891a406e8821
Henrique Morais <rikeaju@hotmail.com>
2022-05-17 10:36:30 -0300

    P05 - Aula 03 - Boas Práticas e Exceções

--- P05/principal.py
+++ P05/principal.py
@@ -1,5 +1,9 @@
-arquivo_contatos = open('dados/contatos.csv', encoding='latin_1')
-
-for linha in arquivo_contatos:
-    print(linha, end='')
+try:
+    with open('dados/nao-existe.csv', encoding='latin_1', mode='w+') as arquivo_contatos:
+        for linha in arquivo_contatos:
+            print(linha, end='')
+except FileNotFoundError:
+    print('Arquivo não encontrado.')
+except PermissionError:
+    print('Sem permissão para abrir o arquivo.')
     
