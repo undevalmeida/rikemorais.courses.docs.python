5d754d9e65e5d0e6a4a88369d408cbde2789cf2e
Henrique Morais <rikeaju@hotmail.com>
2022-05-17 14:33:35 -0300

    P05 - Aula 05 - Convertendo os Contatos

--- P05/principal.py
+++ P05/principal.py
@@ -1,9 +1,17 @@
+import contatos_utils
+
 try:
-    with open('dados/nao-existe.csv', encoding='latin_1', mode='w+') as arquivo_contatos:
-        for linha in arquivo_contatos:
-            print(linha, end='')
+    # contatos = contatos_utils.csv_para_contatos('dados/contatos.csv')
+    # contatos_utils.contatos_para_pickle(contatos, 'dados/contatos.pickle')
+
+    # contatos = contatos_utils.pickle_para_contatos('dados/contatos.pickle')
+    # contatos_utils.contatos_para_json(contatos, 'dados/contatos.json')
+
+    contatos = contatos_utils.json_para_contatos('dados/contatos.json')
+
+    for contato in contatos:
+        print(f'{contato.id} - {contato.nome} - {contato.email}')
 except FileNotFoundError:
-    print('Arquivo não encontrado.')
+    print('Arquivo não encontrado')
 except PermissionError:
-    print('Sem permissão para abrir o arquivo.')
-    
+    print('Sem permissão de escrita')
--- P05/testa_io.py
+++ P05/testa_io.py
@@ -1,12 +1,14 @@
-arquivo = open('dados/contatos.csv', encoding='latin-1', mode='a+')
+arquivo = open('dados/contatos-escrita.csv', encoding='latin_1', mode='a+')
 
 print(type(arquivo.buffer))
 
-texto_em_bytes = bytes('Esse é um texto em bytes', 'latin-1')
+
+texto_em_bytes = bytes('Esse é um texto em bytes', 'latin_1')
 # print(texto_em_bytes)
 # print(type(texto_em_bytes))
 
-contato = bytes('15,Verônica,veronica@veronica.com.br\n', 'latin-1')
+contato = bytes('15,Verônica,veronica@veronica.com.br\n', 'latin_1')
 arquivo.buffer.write(contato)
+
 
 arquivo.close()
