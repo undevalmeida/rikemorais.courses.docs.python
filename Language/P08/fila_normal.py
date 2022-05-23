1387bc1b1728ef61f5ecd41e0eb557157fda9583
Henrique Morais <rikeaju@hotmail.com>
2022-05-23 11:25:20 -0300

    P08 - Lesson 03 - Template Method

--- Language/P08/fila_normal.py
+++ Language/P08/fila_normal.py
@@ -1,25 +1,11 @@
-class FilaNormal:
-    codigo : int = 0
-    fila = []
-    clintesatendidos = []
-    senhaatual : str = ""
+from fila_base import FilaBase
 
-    def gerasenhaatual(self) -> None:
-        self.senhaatual = f'NM{self.codigo}'
-        
-    def resetafila(self) -> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
-            
-    def atualizafila(self) -> None:
-        self.resetafila()
-        self.gerasenhaatual()
-        self.fila.append(self.senhaatual)
-        
-    def chama_cliente(self, caixa:str) -> str:
-        cliente_atual:str = self.fila.pop(0)
-        self.clintesatendidos.append(cliente_atual)
+
+class FilaNormal(FilaBase):
+    def gera_senha_atual(self) -> None:
+        self.senha_atual = f'NM{self.codigo}'
+
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}'
-    
--- Language/P08/fila_prioritaria.py
+++ Language/P08/fila_prioritaria.py
@@ -1,43 +1,33 @@
-from typing import Any
+from fila_base import FilaBase
 
 
-class FilaPrioritaria:
-    codigo: int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ""
-
+class FilaPrioritaria(FilaBase):
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR{self.codigo}'
 
-    '''
-    
-    '''
-    def reseta_fila(self)-> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
+    def chama_cliente(self, caixa: int) -> None:
+        display = []
+        cliente_atual = self.fila.pop(0)
+        display.append(f'Cliente: ]{cliente_atual} - Caixa {caixa}')
 
-    def atualiza_fila(self)-> None:
-        self.reseta_fila()
-        self.gera_senha_atual()
-        self.fila.append(self.senha_atual)
-    
-    def chama_cliente(self, caixa: int)-> str:
-        cliente_atual: str = self.fila.pop(0)
+        if len(self.fila) >= 3:
+            display.append(f'Próximo: {self.fila[0]}')
+            display.append(f'Fique atento: {self.fila[1]}')
+
         self.clientes_atendidos.append(cliente_atual)
-        return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}')
+
+        return display
         
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
+    def estatistica(self, dia: str, agencia: str, flag: str) -> dict:
         if flag != 'detail':
-            estatistica = {f'{agencia}-{dia}': {len(self.clientes_atendidos)}}
+            estatistica = {f'{agencia} - {dia}': len(self.clientes_atendidos)}
         else:
             estatistica = {}
             estatistica['dia'] = dia
             estatistica['agencia'] = agencia
             estatistica['clientes atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_de_clientes_atendidos'] = len(self.clientes_atendidos)
+            estatistica['quantidade de clientes atendidos'] = (
+                len(self.clientes_atendidos)
+            )   
             
         return estatistica
-        
