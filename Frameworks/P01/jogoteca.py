eb6bbd4d95458a9eee43f381c4248bbb4e124995
Henrique Morais <rikeaju@hotmail.com>
2022-05-25 16:20:25 -0300

    P01 - Lesson 02 - Listing Games and Using Flask

--- Frameworks/P01/jogoteca.py
+++ Frameworks/P01/jogoteca.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 
 def ola():
-    return render_template('lista.html')
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = Jogo('Mortal Combate', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
 
 app.run()
