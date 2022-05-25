261d36a9be580f2ed45be26e89cf7c90f31ab8eb
Henrique Morais <rikeaju@hotmail.com>
2022-05-25 19:55:44 -0300

    P01 - Lesson 05 - Authentication

--- Frameworks/P01/jogoteca.py
+++ Frameworks/P01/jogoteca.py
@@ -1,26 +1,26 @@
-from unicodedata import category
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-jogo3 = Jogo('Mortal Combate', 'Luta', 'PS2')
+jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():
-    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html', titulo = 'Novo Jogo')
+    return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
 def criar():
@@ -30,6 +30,25 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
-    
 
-app.run(debug = True)
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')
+
+app.run(debug=True)
