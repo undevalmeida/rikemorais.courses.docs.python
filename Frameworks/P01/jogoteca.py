670fe387731059a48bdae70906db73d365400cd6
Henrique Morais <rikeaju@hotmail.com>
2022-05-25 17:30:46 -0300

    P01 - Lesson 03 - Creating a New Game

--- Frameworks/P01/jogoteca.py
+++ Frameworks/P01/jogoteca.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from unicodedata import category
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,15 +7,29 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Combate', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
-@app.route('/inicio')
-
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Combate', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo = 'Jogos', jogos = lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo = 'Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
+    
+
+app.run(debug = True)
