8bcd1797634dc4a0df64e6f02e407264680c391b
Henrique Morais <rikeaju@hotmail.com>
2022-05-28 13:42:45 -0300

    P02 - Lesson 05 - Finished

--- Frameworks/P02/jogoteca.py
+++ Frameworks/P02/jogoteca.py
@@ -1,129 +1,13 @@
-import os
-import time
-from flask import Flask, render_template, request, redirect, send_from_directory, session, flash, url_for
+from flask import Flask
+
 from flask_mysqldb import MySQL
-from dao import JogoDao, UsuarioDao
-from models import Jogo, Usuario
 
 app = Flask(__name__)
-app.secret_key = 'alura'
-
-app.config['MYSQL_HOST'] = "127.0.0.1"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "admin"
-app.config['MYSQL_DB'] = "jogoteca"
-app.config['MYSQL_PORT'] = 3306
-app.config['UPLOAD_PATH'] = \
-    os.path.dirname(os.path.abspath(__file__)) + '/uploads'
+app.config.from_pyfile('config.py')
 
 db = MySQL(app)
 
-jogo_dao = JogoDao(db)
-usuario_dao = UsuarioDao(db)
+from views import *
 
-@app.route('/')
-def index():
-    lista = jogo_dao.listar()
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
-
-
-@app.route('/novo')
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
-
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    jogo = jogo_dao.salvar(jogo)
-
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-
-@app.route('/editar/<int:id>')
-def editar(id):
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('editar')))
-    jogo = jogo_dao.busca_por_id(id)
-    nome_imagem =  recupera_imagem(id)
-    capa_jogo = f'capa{id}.jpg'
-    return render_template('editar.html', titulo='Editando jogo', jogo=jogo,
-                                capa_jogo = nome_imagem)
-
-
-@app.route('/atualizar', methods=['POST',])
-def atualizar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console, id=request.form['id'])
-    jogo_dao.salvar(jogo)
-
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    deleta_arquivo(jogo.id)
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima)
-
-
-@app.route('/autenticar', methods=['POST',])
-def autenticar():
-    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
-    if usuario:
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.id
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-    else :
-        flash('Não logado, tente de novo!')
-        return redirect(url_for('login'))
-
-
-@app.route('/deletar/<int:id>')
-def deletar(id):
-    jogo_dao.deletar(id)
-    flash('O jogo foi removido com sucesso!')
-    return redirect(url_for('index'))
-
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-    flash('Nenhum usuário logado!')
-    return redirect(url_for('index'))
-
-
-@app.route('/uploads/<nome_arquivo>')
-def imagem(nome_arquivo):
-    return send_from_directory('uploads', nome_arquivo)
-
-
-def recupera_imagem(id):
-    for nome_arquivo in os.listdir(app.config['UPLOAD_PATH']):
-        if f'capa{id}' in nome_arquivo:
-            return nome_arquivo
-
-
-def deleta_arquivo(id):
-    arquivo = recupera_imagem(id)
-    os.remove(os.path.join(app.config['UPLOAD_PATH'], arquivo))
-
-
-app.run(debug = True)
+if __name__ == '__main__':
+    app.run(debug=True)
