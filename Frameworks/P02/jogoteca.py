9d34939d06b0ca8ecff07a2dea928d02e7698767
Henrique Morais <rikeaju@hotmail.com>
2022-05-28 11:10:25 -0300

    P02 - Lesson 03 - File Upload

--- Frameworks/P02/jogoteca.py
+++ Frameworks/P02/jogoteca.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for
+import os
+from flask import Flask, render_template, request, redirect, send_from_directory, session, flash, url_for
 from flask_mysqldb import MySQL
 from dao import JogoDao, UsuarioDao
 from models import Jogo, Usuario
@@ -11,6 +12,8 @@ app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "admin"
 app.config['MYSQL_DB'] = "jogoteca"
 app.config['MYSQL_PORT'] = 3306
+app.config['UPLOAD_PATH'] = \
+    os.path.dirname(os.path.abspath(__file__)) + '/uploads'
 
 db = MySQL(app)
 
@@ -32,11 +35,15 @@ def novo():
 
 @app.route('/criar', methods=['POST',])
 def criar():
-    nome = request. form['nome']
-    categoria = request. form['categoria']
-    console = request. form['console']
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
     jogo = Jogo(nome, categoria, console)
-    jogo_dao.salvar(jogo)
+    jogo = jogo_dao.salvar(jogo)
+
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    arquivo.save(f'{upload_path}/capa{jogo.id}.jpg')
     return redirect(url_for('index'))
 
 
@@ -45,7 +52,8 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = jogo_dao.busca_por_id(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo = jogo)
+    return render_template('editar.html', titulo='Editando jogo', jogo=jogo,
+                                capa_jogo = f'capa{id}.jpg')
 
 
 @app.route('/atualizar', methods=['POST',])
@@ -90,6 +98,11 @@ def logout():
     session['usuario_logado'] = None
     flash('Nenhum usuário logado!')
     return redirect(url_for('index'))
+
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
 
 
 app.run(debug = True)
