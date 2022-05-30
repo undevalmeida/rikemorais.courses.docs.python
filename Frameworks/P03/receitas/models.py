90562f3c79abbab27764022422eed498e9f08395
Henrique Morais <rikeaju@hotmail.com>
2022-05-30 14:50:30 -0300

    P03 - Lesson 05 - Django Admin

--- Frameworks/P03/receitas/models.py
+++ Frameworks/P03/receitas/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class Receita(models.Model):
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
-    modo_preparo = models.TextField
+    modo_preparo = models.TextField()
     tempo_preparo = models.IntegerField()
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
