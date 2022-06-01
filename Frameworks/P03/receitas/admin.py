5b690c0ea9d550608b03afec5e7486e490842c9c
Henrique Morais <rikeaju@hotmail.com>
2022-06-01 17:08:49 -0300

    P04 - Lesson 04 - Authorization

--- Frameworks/P03/receitas/admin.py
+++ Frameworks/P03/receitas/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from .models import Receita
 
-
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'pessoa', 'nome_receita', 'categoria',
-                    'tempo_preparo', 'publicada')
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada')
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
-    list_editable = ('publicada', 'tempo_preparo', 'pessoa', 'categoria')
-    list_per_page = 50
-
+    list_editable = ('publicada',)
+    list_per_page = 5
 
 admin.site.register(Receita, ListandoReceitas)
