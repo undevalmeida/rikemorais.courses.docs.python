74aaa80b41264cd803f3410b9b2f0c8cae1e1f50
Henrique Morais <rikeaju@hotmail.com>
2022-06-02 16:49:10 -0300

    P05 - Lesson 02 - Forms

--- Frameworks/P03/usuarios/views.py
+++ Frameworks/P03/usuarios/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não ficar em branco!')
+            return redirect('cadastro')
+        if not email.strip():
+            print('O campo e-mail não pode ficar em branco!')
+            return redirect('cadastro')
+        if senha != senha2:
+            print('As senhas não são iguais!')
+            return redirect('cadastro')
+        if User.objects.filter(email=email).exists():
+            print('E-mail já cadastrado!')
+            return redirect('cadastro')
+        user = User.objects.create_user(
+            username=nome, email=email, password=senha)
+        user.save()
+        print('Usuário cadastrado com sucesso!')
+        return redirect('login')
+    else:
+        return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
