6ef4dfbfa2a68ce462c5d8f071ceb2dd88bbdde0
Henrique Morais <rikeaju@hotmail.com>
2022-06-03 14:19:17 -0300

    P05 - Lesson 04 - Recipe Table

--- Frameworks/P03/receitas/models.py
+++ Frameworks/P03/receitas/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from datetime import datetime
-from pessoas.models import Pessoa
+from django.contrib.auth.models import User
+
 
 class Receita(models.Model):
-    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
+    pessoa = models.ForeignKey(User, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -13,5 +14,6 @@ class Receita(models.Model):
     date_receita = models.DateTimeField(default=datetime.now, blank=True)
     foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
+
     def __str__(self):
         return self.nome_receita
