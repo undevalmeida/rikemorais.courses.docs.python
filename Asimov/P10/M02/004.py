53a386cfbe8602f30286de7767e0c554ea727eea
Henrique Morais <rikeaju@hotmail.com>
2022-06-05 13:40:57 -0300

    P10 - M02 - Building Dashboards with Dash

---
+++ Asimov/P10/M02/004.py
@@ -0,0 +1,66 @@
+# Decorators
+
+# 1 - Definindo variáveis como funções
+
+
+def soma_um(numero):
+    return numero + 1
+
+
+f1 = soma_um
+f1(1)
+
+# 2 - Definindo funções dentro de outras funções
+
+
+def soma_um(numero):
+    def adiciona_um(numero):
+        return numero + 1
+    return adiciona_um(numero)
+
+
+soma_um(4)
+
+# 3 - Passando como argumentos de outras funções
+
+
+def soma_um(numero):
+    return numero + 1
+
+
+def function_call(function):
+    numero_to_add = 5
+    return function(numero_to_add)
+
+
+function_call(soma_um)
+
+# 4 - Funções retornando outras funções
+
+
+def funcao_ola():
+    def diga_oi():
+        return 'Oi'
+    return diga_oi
+
+
+hello = funcao_ola()
+hello()
+
+# 5 - Exemplo
+
+
+def decorador_maiusculo(function):
+    def wrapper():
+        func = function()
+        cria_maiusculo = func.upper()
+        return cria_maiusculo
+    return wrapper
+
+
+def diga_oi():
+    return 'Hello There'
+
+
+funcao_decorada = decorador_maiusculo(diga_oi)
+funcao_decorada()
