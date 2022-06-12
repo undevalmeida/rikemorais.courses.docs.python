634156db161338dd7914f186c84624b2b838b52d
Henrique Morais <rikeaju@hotmail.com>
2022-06-12 11:36:10 -0300

    P07 - Novos Campos

--- Alura/Frameworks/P07/passagens/forms.py
+++ Alura/Frameworks/P07/passagens/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
+from passagens.classe_viagem import tipos_de_classe
 
 
 class PassagemForms(forms.Form):
@@ -7,3 +9,17 @@ class PassagemForms(forms.Form):
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(
+        label='Data da Pesquisa', disabled=True, initial=datetime.today())
+    classe_viagem = forms.ChoiceField(
+        label='Classe de Viagem', choices=tipos_de_classe)
+    information = forms.CharField(
+        label='Informações adicionais',
+        max_length=200,
+        widget=forms.Textarea(),
+        required=False
+    )
+    email = forms.EmailField(
+        label='E-mail',
+        max_length=150
+    )
