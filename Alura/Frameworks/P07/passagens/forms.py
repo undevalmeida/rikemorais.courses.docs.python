405b0a4fca7b53eba8439e8194df91fe4b404472
Henrique Morais <rikeaju@hotmail.com>
2022-06-13 11:37:28 -0300

    P07 - Validations

--- Alura/Frameworks/P07/passagens/forms.py
+++ Alura/Frameworks/P07/passagens/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classe
+from passagens.validation import *
 
 
 class PassagemForms(forms.Form):
@@ -10,16 +11,31 @@ class PassagemForms(forms.Form):
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
     data_pesquisa = forms.DateField(
-        label='Data da Pesquisa', disabled=True, initial=datetime.today())
+        label='Data da pesquisa', disabled=True, initial=datetime.today)
     classe_viagem = forms.ChoiceField(
-        label='Classe de Viagem', choices=tipos_de_classe)
-    information = forms.CharField(
-        label='Informações adicionais',
+        label='Classe do vôo', choices=tipos_de_classe)
+    informacoes = forms.CharField(
+        label='Informações extras',
         max_length=200,
         widget=forms.Textarea(),
         required=False
     )
-    email = forms.EmailField(
-        label='E-mail',
-        max_length=150
-    )
+    email = forms.EmailField(label='Email', max_length=150)
+
+    def clean(self):
+        origem = self.cleaned_data.get('origem')
+        destino = self.cleaned_data.get('destino')
+        data_ida = self.cleaned_data.get('data_ida')
+        data_volta = self.cleaned_data.get('data_volta')
+        data_pesquisa = self.cleaned_data.get('data_pesquisa')
+        lista_de_erros = {}
+        campo_tem_algum_numero(origem, 'origem', lista_de_erros)
+        campo_tem_algum_numero(destino, 'destino', lista_de_erros)
+        origem_destino_iguais(origem, destino, lista_de_erros)
+        data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros)
+        data_ida_menor_data_de_hoje(data_ida, data_pesquisa, lista_de_erros)
+        if lista_de_erros is not None:
+            for erro in lista_de_erros:
+                mensagem_erro = lista_de_erros[erro]
+                self.add_error(erro, mensagem_erro)
+        return self.cleaned_data
